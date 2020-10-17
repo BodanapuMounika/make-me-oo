2e8fd06fe8dc334f852717c6113057db4ebcd5e6
Mounika Bodanapu <mounimounika286@gmail.com>
2020-10-17 14:00:51 +0530

    modified

--- src/main/java/org/oop/DistanceAndDirectionCalculator.java
+++ src/main/java/org/oop/DistanceAndDirectionCalculator.java
@@ -4,21 +4,18 @@ public class DistanceAndDirectionCalculator {
     public static double xDifference;
     public static double yDifference;
 
-    public DistanceAndDirectionCalculator(Point start, Point end) {
-        setCoordinatesDifference (start, end);
-    }
-
     public void setCoordinatesDifference(Point start, Point end) {
         xDifference = end.getXdifference (start);
         yDifference = end.getYdifference (start);
     }
 
-    public static double distanceOfPoints() {
-
+    public static double distance(Point start, Point end) {
+	setCoordinatesDifference( start,  end)
         return Math.sqrt (square (xDifference) + square (yDifference));
     }
 
-    public static double directionOfPoints() {
+    public static double direction(Point start, Point end) {
+	setCoordinatesDifference( start,  end)
         return Math.atan2 (yDifference, xDifference);
     }
 
