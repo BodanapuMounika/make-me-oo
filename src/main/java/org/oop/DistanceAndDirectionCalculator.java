13835b252aa060a812163f6546b227ccc1861f75
Mounika Bodanapu <mounimounika286@gmail.com>
2020-10-17 16:09:06 +0530

    duplcate statements are removed and difference between two points method is extracted as behaviour of point class

--- src/main/java/org/oop/DistanceAndDirectionCalculator.java
+++ src/main/java/org/oop/DistanceAndDirectionCalculator.java
@@ -1,15 +1,30 @@
 package org.oop;
 
 public class DistanceAndDirectionCalculator {
-    public static double distance(Point from, Point to) {
-        double xDistance = to.getX() - from.getX();
-        double yDistance = to.getY() - from.getY();
-        return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
+    public static double xDifference;
+    public static double yDifference;
+
+    public DistanceAndDirectionCalculator(Point start, Point end) {
+        setCoordinatesDifference (start, end);
     }
 
-    public static double direction(Point from, Point to) {
-        double xDistance = to.getX() - from.getX();
-        double yDistance = to.getY() - from.getY();
-        return Math.atan2(yDistance, xDistance);
+    public void setCoordinatesDifference(Point start, Point end) {
+        xDifference = end.getXdifference (start);
+        yDifference = end.getYdifference (start);
     }
+
+    public static double distanceOfPoints() {
+
+        return Math.sqrt (square (xDifference) + square (yDifference));
+    }
+
+    public static double directionOfPoints() {
+        return Math.atan2 (yDifference, xDifference);
+    }
+
+    private  double square(double xDifference) {
+        return Math.pow (xDifference, 2);
+    }
+
+
 }
